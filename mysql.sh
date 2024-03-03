567dcde8949ef4233f6f05117fc8d41f8c91f91d
Vinuthna <vinnuch2408@gmail.com>
2024-03-02 20:48:46 -0800

    roboshop shell script

---
+++ mysql.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+ID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+TIMESTAMP=$(date +%F-%H-%M-%S)
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
+
+echo "script stareted executing at $TIMESTAMP" &>> $LOGFILE
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2 ... $R FAILED $N"
+    else
+        echo -e "$2 ... $G SUCCESS $N"
+    fi
+}
+
+if [ $ID -ne 0 ]   #if roboshop user does not exist, then it is failure
+then
+    echo -e "$R ERROR:: Please run this script with root access $N"
+    exit 1
+else
+    echo "You are root user"
+fi
+
+dnf module disable mysql -y
+
+VALIDATE $? "Disable current Mysql version"
+
+cp mysql.repo /etc/yum.repos.d/mysql.repo
+
+VALIDATE $? "Copied Mysql repo"
+
+dnf install mysql-community-server -y
+
+VALIDATE $? "installing mysql server"
+
+systemctl enable mysqld
+
+VALIDATE $? "Enable mysql"
+
+systemctl start mysqld
+
+VALIDATE $? "start mysql"
+
+mysql_secure_installation --set-root-pass RoboShop@1
+
+VALIDATE "Setting mysql root password"
+
+
