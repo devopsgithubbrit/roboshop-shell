647d25f1f75e799c8624b4e293efd5b586ea4c9c
Vinuthna <vinnuch2408@gmail.com>
2024-06-10 12:39:34 -0700

    shell

--- roboshop.sh
+++ roboshop.sh
@@ -3,7 +3,7 @@
 AMI=ami-0b4f379183e5706b9
 SG_ID=sg-0541eee51da2719f0
 INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "web")
-ZONE_ID=Z09079071I9PB2SGNM2HI 
+ZONE_ID=Z09079071I9PB2SGNM2HI
 DOMAIN_NAME="chintu.cloud"
 
 for i in "${INSTANCES[@]}"
@@ -15,7 +15,7 @@ do
         INSTANCE_TYPE="t2.micro"
     fi
 
-    IP_ADDRESS=$(aws ec2 run-instances --image-id ami-0f3c7d07486cad139 --instance-type $INSTANCE_TYPE --security-group-ids sg-0541eee51da2719f0 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'Instances[0].PrivateIpAddress' --output text)
+    IP_ADDRESS=$(aws ec2 run-instances --image-id ami-0b4f379183e5706b9 --instance-type $INSTANCE_TYPE --security-group-ids sg-0541eee51da2719f0 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'Instances[0].PrivateIpAddress' --output text)
     echo "$i: $IP_ADDRESS"
 
     
