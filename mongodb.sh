eaee598721829c4df27fd07eb9fa5d8058c0fdef
devopsgithubbrit <vinnuch2408@gmail.com>
2024-07-17 08:14:06 -0700

    roboshop

--- mongodb.sh
+++ mongodb.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 
 ID=$(id -u)
-TIMESTAMP=$(date +%F-%H-%M-%S)
 R="\e[31m"
 G="\e[32m"
+Y="\e[33m"
 N="\e[0m"
 
+TIMESTAMP=$(date +%F-%H-%M-%S)
 LOGFILE="/tmp/$0-$TIMESTAMP.log"
 
-echo "Script started executing at $TIMESTAMP" &>> $LOGFILE
+echo "script stareted executing at $TIMESTAMP" &>> $LOGFILE
 
 VALIDATE(){
     if [ $1 -ne 0 ]
     then
-        echo -e "ERROR:: $2 ... $R FAILED $N"
+        echo -e "$2 ... $R FAILED $N"
         exit 1
     else
         echo -e "$2 ... $G SUCCESS $N"
@@ -26,28 +27,29 @@ then
     exit 1 
 else
     echo "You are root user"
-fi
+fi 
 
-cp mongo.repo /etc/yum.repos.d/mongo.repo  &>> $LOGFILE
+cp mongo.repo /etc/yum.repos.d/mongo.repo &>> $LOGFILE
 
-VALIDATE $? "Repository Setup"
+VALIDATE $? "Copied MongoDB Repo"
 
-dnf install mongodb-org -y  &>> $LOGFILE
+dnf install mongodb-org -y &>> $LOGFILE
 
-VALIDATE $? "Installing MOngoDB"
+VALIDATE $? "Installing MongoDB"
 
 systemctl enable mongod &>> $LOGFILE
 
 VALIDATE $? "Enabling MongoDB"
 
-systemctl start mongod  &>> $LOGFILE
+systemctl start mongod &>> $LOGFILE
 
-VALIDATE $ "Starting MongoDB"
+VALIDATE $? "Starting MongoDB"
 
-sed -i '/s/127.0.0.1/0.0.0.0/g'  /etc/mongod.conf  &>> $LOGFILE
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf &>> $LOGFILE
 
-VALIDATE $? "port change"
+VALIDATE $? "Remote access to MongoDB"
 
-systemctl restart mongod    &>> $LOGFILE
+systemctl restart mongod &>> $LOGFILE
 
 VALIDATE $? "Restarting MongoDB"
+
